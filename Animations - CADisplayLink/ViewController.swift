9ae28b7546c4702474ed033747dc4bbc7564b56f
Christian Lorenzo <clbmiami2004@gmail.com>
2020-01-26 21:57:39 -0800

    Created a label programmatically. Centered the label in the viewController, and added code for the label to show the count incrementing from 0 to 100 for now.

--- Animations - CADisplayLink/ViewController.swift
+++ Animations - CADisplayLink/ViewController.swift
@@ -10,14 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let
+    let countingLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = "1234"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(countingLabel)
+        countingLabel.frame = view.frame
+        
+        //Create my CADisplayLink here:
+        let displayLink = CADisplayLink(target: self, selector: #selector(handleUpdate))
+        
+        displayLink.add(to: .main, forMode: .default)
     }
+    
+    //Starting the count from 0.
+    var startValue = 0
+    let endValue = 12000
 
+    @objc func handleUpdate() {
+//        let seconds = Date().timeIntervalSince1970
+//        print(seconds)
+//        self.countingLabel.text = "\(seconds)"
+        
+        self.countingLabel.text = "\(startValue)"
+        startValue += 1
+        
+        if startValue > endValue {
+            startValue = endValue
+        }
+    }
 
 }
 
